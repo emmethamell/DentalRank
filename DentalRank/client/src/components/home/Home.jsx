4445f0d77177f5efc254e692462dba3f4f2435fe
emmet hamell <emmethamell@yahoo.com>
2024-05-19 17:29:52 -0400

    fix routing

--- DentalRank/client/src/components/home/Home.jsx
+++ DentalRank/client/src/components/home/Home.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Footer from "../footer/Footer";
 import "./home.css";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 const Home = () => {
-    return (
-        <>
-        <div className="home">
-            <div className="jumbotron">
-                <h1 className="display-4">Rank your Dental Schools</h1>
-                <p className="lead">Choose what matters most to you in order to rank dental schools</p>
-                <a className="btn btn-primary btn-lg" href="/rank" role="button">Create your Ranking</a>
-            </div>
+  return (
+    <>
+      <div className="home">
+        <div className="jumbotron">
+          <h1 className="display-4">Rank your Dental Schools</h1>
+          <p className="lead">
+            Choose what matters most to you in order to rank dental schools
+          </p>
+          <a className="btn btn-primary btn-lg" role="button">
+            <Nav.Link className="nav-link" as={Link} to="/rank">
+              Create your Ranking
+            </Nav.Link>
+          </a>
         </div>
-        <div className="footer-home">
-            <Footer />
-        </div>
-        </>
-    );
+      </div>
+      <div className="footer-home">
+        <Footer />
+      </div>
+    </>
+  );
 };
 
-export default Home
+export default Home;
