ede7736f56c97b7b412427d887ee3314587c3efa
emmet hamell <emmethamell@yahoo.com>
2024-11-14 00:54:54 -0500

    generate sitemap

--- DentalRank/client/vite.config.js
+++ DentalRank/client/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.dentalrank.us',
+      dynamicRoutes: [
+        '/',       
+        '/rank',   
+        '/signin', 
+        '/signup',
+      ],
+      readable: true, 
+      changefreq: 'daily',
+      priority: 1.0,
+    }),
+  ],
 })
